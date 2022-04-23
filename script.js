39b410d4def3db5e0af553ab041fb5fb4bf64659
Kartohan <Kartohan@gmail.com>
2022-04-23 12:38:38 +0300

    Fix computerSelection

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 function computerPlay() {
-    let random = Math.round(Math.random() * 10)
-    if (random <= 3) {
+    let random = Math.ceil(Math.random() * 3)
+    if (random === 1) {
         return("Rock")
-    } else if (random <= 6) {
+    } else if (random === 2) {
         return("Paper")
-    } else {
+    } else if (random === 3) {
         return("Scissors")
     }
 }
 
 function round (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
     if (playerSelection == computerSelection) {
         return("Tie game!")
     } else if (playerSelection != computerSelection) {
-        if (playerSelection === "Rock" && computerSelection === "Paper") {
+        if (playerSelection === "rock" && computerSelection === "Paper") {
             return("You Lose! Paper beats Rock!")
-        } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        } else if (playerSelection === "rock" && computerSelection === "Scissors") {
             return("You Win! Rock beats Scissors!")
-        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        } else if (playerSelection === "paper" && computerSelection === "Rock") {
             return("You Win! Paper beats Rock!")
-        } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        } else if (playerSelection === "paper" && computerSelection === "Scissors") {
             return("You Lose! Scissors beats Paper!")
-        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        } else if (playerSelection === "scissors" && computerSelection === "Paper") {
             return("You Win! Scissors beats Paper!")
-        } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        } else if (playerSelection === "scissors" && computerSelection === "Rock") {
             return("You Lose! Rock beats Scissors")
         }
     }
